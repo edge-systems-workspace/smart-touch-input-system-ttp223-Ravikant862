ba19154f6bf3a6db86ff233f6dedcbfdafbef15c
ravi <ravjkant.gla_cs.aiml24@gla.ac.in>
2026-02-19 08:47:37 +0530

    Define touch sensor digital pin (Use pin 2)

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,27 @@
  * and displays structured output via Serial Monitor.
  */
 
+
  // TODO 1:
  // Define touch sensor digital pin (Use pin 2)
+ int touch_Pin=12;
 
  // TODO 2:
  // Create variable to store touch state
+int led_Pin=13;
 
 void setup() {
 
     // TODO 3:
+    Serial.begin(9600);
     // Initialize Serial communication (9600 baud rate)
 
     // TODO 4:
     // Configure touch pin as INPUT
+    pinMode(touch_Pin,INPUT);
 
     // TODO 5:
+    pinMode(led_Pin,OUTPUT);
     // Print system initialization message
 }
 
@@ -34,6 +40,15 @@ void loop() {
     // Read digital value from touch sensor
 
     // TODO 7:
+    int state=digitalRead(touch_Pin);
+    if(state==0){
+        digitalWrite(13,HIGH);
+        Serial.println("DETECTED!!");}
+
+    else{
+        digitalWrite(13,LOW);
+        Serial.println("NOT DETECTED!!");
+    }
     // If touch detected (HIGH)
     //     Print "Touch Detected"
     // Else
